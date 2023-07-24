7dd9944f5eb8a0d6a87248379c99fdae605ac539
null <null>
2023-07-24 23:13:08 +0000

    first

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app =express()
+
+app.use("/",(req,res)=>{
+    console.log("hit");
+    res.json({"hello":"ok"});
+})
+
+app.listen(3000,()=>{console.log("started")})
