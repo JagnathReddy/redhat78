f49f282449f9e88a413d1edff5e025c6015b15b3
null <null>
2023-07-26 11:35:26 +0000

    added express

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require("express")
+
+const app=express()
+
+app.use(express.json)
+
+app.use((req,res,next)=>{
+    console.log(req.body)
+    next()
+})
+
+app.use("/start",(req,res)=>{
+    console.log("Successfully tested")
+    res.json({"status":"ok"})
+})
+
+app.listen(8080,()=>{
+    console.log("started listening at port 8000");
+})
