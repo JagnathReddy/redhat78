269991d78a7d4046228d08539c3a4af744d7f608
jagnathreddy <98260819+JagnathReddy@users.noreply.github.com>
2023-07-28 04:27:02 +0530

    added functionality = pdf add and pdf get

--- controller/docController.js
+++ controller/docController.js
@@ -1,19 +1,49 @@
-import asyncHandler from "express-async-handler";
-const storeDocument=asyncHandler(async(req,res)=>{
+const path = require('path');
+const express = require('express');
+const fs=require('fs')
+const cos = require("../cloud object storage/cos.js");
+const PDFDocument = require('pdfkit');
+module.exports.storeDocument=(req,res)=>{
     const {documentBase64,key}=req.body;
-    var params = {
-        Body: documentBase64, 
-        Bucket: "", 
-        Key: key, 
-    };
-    await s3.putObject(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack); 
-            res.status(401).json({status:"error",error:err,message:err.stack})
-        }          // an error occurred
-        else{     
-            console.log(data); 
-            res.status(200).json({status:"success",data})
-        }          // successful response
-    });
-})
+        console.log(documentBase64);
+        console.log(key)
+            var params = {
+                Body: documentBase64.toString(),
+                Bucket: "cloud-object-storage-cos-standard-wwf", 
+                Key: key
+            };
+            cos.putObject(params, function(err, data) {
+                if (err) {
+                    console.log(err, err.stack); 
+                    res.status(401).json({status:"error",error:err,message:err.stack})
+                }          // an error occurred
+                else{     
+                    console.log(data); 
+                    res.status(200).json({status:"success",data})
+                }          // successful response
+            });
+            
+}
+
+
+
+
+
+module.exports.getDocument=(req,res)=>{
+    cos.getObject({Key:req.params.keyId,Bucket:"cloud-object-storage-cos-standard-wwf"})
+    .on('complete',(response)=>{
+        // Get the base64 encoded PDF from your server
+        pdfBase64="non"
+        if(response.data.Body)
+        var pdfBase64 = response.data.Body?.toString();
+        
+        // Set the content type and send the base64 encoded PDF as the response body
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(Buffer.from(pdfBase64, 'base64'));
+    }).on('error',(error)=>{
+        res.status(404).json(error)
+    })
+    .send()
+}
+
+
