11b85593d5073ccee13e5e3e3d775d7575de9890
jagnathreddy <98260819+JagnathReddy@users.noreply.github.com>
2023-07-28 11:30:59 +0530

    created env

--- cloud object storage/cos.js
+++ cloud object storage/cos.js
@@ -1,9 +1,9 @@
 const S3 =require("ibm-cos-sdk/clients/s3.js");
-
+require('dotenv').config()
 
 var config = {
     endpoint: 's3.jp-tok.cloud-object-storage.appdomain.cloud',
-    apiKeyId: '0xvvyEIWYXf8rceC3khbI4sD_tVyAp-7qqnZ8czJIlHp',
+    apiKeyId: process.env.KEY_ID,
     serviceInstanceId: 'crn:v1:bluemix:public:cloud-object-storage:global:a/dbad7cc0c6a642f892afdcb81fac6df4:3bf9d86f-811f-480b-af51-ab1401c5e643::',
     signatureVersion: 'iam',
     region:"jp-tok"
